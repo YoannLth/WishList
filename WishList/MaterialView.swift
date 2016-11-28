43825cb640368a6ac6dc3ed1fa9370f9cb02a51b
YoannLTH <yoann.lathuiliere@gmail.com>
2016-11-28 16:08:52 +0100

    Set UI

---
+++ WishList/MaterialView.swift
@@ -0,0 +1,37 @@
+//
+//  MaterialView.swift
+//  WishList
+//
+//  Created by yoann lathuiliere on 28/11/2016.
+//  Copyright © 2016 ylth. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+
+extension UIView {
+  @IBInspectable var materialDesign: Bool{
+    get{
+      return materialKey
+    }
+    set{
+      materialKey = newValue
+      
+      if materialKey{
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = 3.0
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 3.0
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowColor = UIColor(red:0.62, green:0.62, blue:0.62, alpha:1.0).cgColor
+      }
+      else{
+        self.layer.cornerRadius = 0
+        self.layer.shadowOpacity = 0
+        self.layer.shadowRadius = 0
+        self.layer.shadowColor = nil
+      }
+    }
+  }
+}
